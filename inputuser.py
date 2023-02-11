33fb6a20e27d71226089f6adf0b126b722793f1b
Bernardo Augusto <bernardo.almeida@uscsonline.com.br>
2023-02-11 15:08:51 -0300

    Given a input a int number for we can convert

--- inputuser.py
+++ inputuser.py
@@ -1,7 +1,13 @@
-calculation_to_seconds = 24 * 60 * 60
-seconds = "segundos"
+calculation_to_seconds = 24 
+hours = "hours"
+
 
 def function_to_calculation(number_of_days):
-    print(f"{number_of_days} dias são {number_of_days * calculation_to_seconds} {seconds}")
- 
-input("Enter a number of days, and I will convert to you in hours")
+    return f"{number_of_days} dias são {number_of_days * calculation_to_seconds} {hours}"
+
+user_input = input("enter a number of day for a can convert in hours for you\n")
+
+user_input_int = int(user_input)
+
+resultofinput = function_to_calculation(user_input_int)
+print(resultofinput)
