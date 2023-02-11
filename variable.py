e4dfee6c8c6f9ab5ca4be49a812b1fa1d129a1c0
Bernardo Augusto <bernardo.almeida@uscsonline.com.br>
2023-02-11 13:22:37 -0300

    using more functions with variables

--- variable.py
+++ variable.py
@@ -2,7 +2,10 @@ calculation_to_seconds = 24 * 60 * 60
 seconds = "segundos"
 
 def function_to_calculation(number_of_days):
-    print(f"1 dia são {number_of_days * calculation_to_seconds} {seconds}")
+    print(f"{number_of_days} dias são {number_of_days * calculation_to_seconds} {seconds}")
  
 
-function_to_calculation(1)
+function_to_calculation(2)
+function_to_calculation(3)
+function_to_calculation(5)
+function_to_calculation(10)
