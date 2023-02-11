c8686cfdd52c972f4e7446cab7d73bf12b56eb7f
Bernardo Augusto <bernardo.almeida@uscsonline.com.br>
2023-02-11 14:21:02 -0300

    Put input in the code

---
+++ inputuser.py
@@ -0,0 +1,7 @@
+calculation_to_seconds = 24 * 60 * 60
+seconds = "segundos"
+
+def function_to_calculation(number_of_days):
+    print(f"{number_of_days} dias são {number_of_days * calculation_to_seconds} {seconds}")
+ 
+input("Enter a number of days, and I will convert to you in hours")
