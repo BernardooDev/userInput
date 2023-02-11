db031cc54722c4ba3aebfe606f25dfda91eda921
Bernardo Augusto <bernardo.almeida@uscsonline.com.br>
2023-02-11 11:51:34 -0300

    new print for def

--- variable.py
+++ variable.py
@@ -1,2 +1,3 @@
 calculation_to_seconds = 24 * 60 * 60
-print(f"24 dias são {24 * calculation_to_seconds}")
+print(f"24 dias são {24 * calculation_to_seconds} segundos")
+print(f"30 dias são {30 * calculation_to_seconds} segundos")
