db8642e7e5f1a96d19d8d35d38e679f4b50527d9
Bernardo Augusto <bernardo.almeida@uscsonline.com.br>
2023-02-11 12:07:53 -0300

    using print inside the function

--- variable.py
+++ variable.py
@@ -1,6 +1,9 @@
-calculation_to_seconds = 24 * 60 * 60)
+calculation_to_seconds = 24 * 60 * 60
 
-print(f"24 dias são {24 * calculation_to_seconds} segundos")
-print(f"24 dias são {40 * calculation_to_seconds} segundos")
-print(f"24 dias são {50 * calculation_to_seconds} segundos")
-print(f"24 dias são {100 * calculation_to_seconds} segundos")
+def function_to_calculation():
+    print(f"24 dias são {24 * calculation_to_seconds} segundos")
+    print(f"40 dias são {40 * calculation_to_seconds} segundos")
+    print(f"50 dias são {50 * calculation_to_seconds} segundos")
+    print(f"110 dias são {100 * calculation_to_seconds} segundos")
+
+function_to_calculation()
